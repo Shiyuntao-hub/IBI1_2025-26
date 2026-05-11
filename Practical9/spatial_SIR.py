62aec98cca1d426d21de1e42b4e9ea20b650d003
Shiyuntao-hub <yuntao.25@intl.zju.edu.cn>
2026-05-11 18:39:51 +0800

    reserve the file again

--- Practical9/spatial_SIR.py
+++ Practical9/spatial_SIR.py
@@ -1,39 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 模型参数
+# paremeters
 size = 100
 beta = 0.3
 gamma = 0.05
 time_steps = 100
 
-# 初始化：全部易感（0）
+# initialize population: 0 for susceptible, 1 for infected, 2 for recovered
 pop = np.zeros((size, size), dtype=int)
 
-# 随机选一个点感染
+# choose a random point to start the outbreak
 outbreak = np.random.choice(size, 2)
 x0, y0 = outbreak[0], outbreak[1]
 pop[x0, y0] = 1
 
-# 绘图初始状态
+# initial plot
 plt.figure(figsize=(6,4), dpi=150)
 plt.imshow(pop, cmap='viridis', interpolation='nearest')
 plt.title('Time 0')
 plt.show()
 
-# 时间循环
+# time circulation
 for step in range(time_steps):
-    # 复制当前状态，避免边改边算出错
+    # copy the current state to avoid simultaneous updates
     current = pop.copy()
-    # 找到所有感染点
+    # find all infected points
     infected = np.argwhere(current == 1)
     
     for (i,j) in infected:
-        # 恢复过程
+        # recovery process
         if np.random.rand() < gamma:
             pop[i,j] = 2
         
-        # 感染8邻域
+        # infection of 8 neighbors
         for di in [-1,0,1]:
             for dj in [-1,0,1]:
                 if di ==0 and dj ==0:
@@ -44,7 +44,7 @@ for step in range(time_steps):
                     if current[ni, nj] == 0 and np.random.rand() < beta:
                         pop[ni, nj] = 1
     
-    # 每10步画一次图
+    # plot every 10 steps
     if step %10 ==0:
         plt.imshow(pop, cmap='viridis', interpolation='nearest')
         plt.title(f'Time {step}')
